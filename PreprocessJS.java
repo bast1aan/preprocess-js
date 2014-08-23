6df24c3a13a35401b37953e667cc5b8f87dfc8a7
Bastiaan Welmers <bastiaan@welmers.net>
2014-08-24 01:02:43 +0200

    Removed unused imports

--- PreprocessJS.java
+++ PreprocessJS.java
@@ -7,11 +7,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 
 public class PreprocessJS {
